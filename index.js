c3306ea5c3eb480d8a3e4cafe090666f903fd7e3
James Earle <j_earle@hotmail.com>
2016-09-21 13:03:41 -0700

    added async calls with JS closure

--- index.js
+++ index.js
@@ -5,10 +5,16 @@
       www.jamesearle.ca
 */
 
-function makeStateButton(state) {
+function makeStateButton(state, isIssue) {
+    var iconClass = {
+        "Opened": "octicon-issue-opened",
+        "Closed": "octicon-issue-closed",
+        "Merged": "octicon-git-pull-request"
+    }
+
     return "<span style='width:83.59px;' class='state state-" + state.toLowerCase() + "'> \
-          <svg aria-hidden='true' class='octicon octicon-issue-opened' height='16' version='1.1' viewBox='0 0 14 16' width='14'><path d='M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1\ 3H6v5h2V4zm0 6H6v2h2v-2z'></path></svg>\
-          " + state + "</span>";
+            <svg aria-hidden='true' class='octicon " + iconClass[state] +"' height='16' version='1.1' viewBox='0 0 14 16' width='14'><path d='M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1\ 3H6v5h2V4zm0 6H6v2h2v-2z'></path></svg>\
+            " + state + "</span>";
 }
 
 // Refresh the page when an input is checked to maintain the state buttons
@@ -24,6 +30,9 @@ function makeStateButton(state) {
 var checklist = document.querySelectorAll("li.task-list-item");
 
 if (checklist.length != 0) {
+
+    var dict = {};
+
     console.log("Fetching information on issues and pull requests...");
 
     for (var i = 0; i < checklist.length; i++) {
@@ -44,34 +53,86 @@ if (checklist.length != 0) {
         var checkbox = elems[0];
         var anchor = elems[1];
 
-        // Make synchronous AJAX (SJAX?) calls to the related issues pages
-        // NOTE asynchronous will iterate the for loop we are in while 
-        // putting ajax calls in the queue, then will call all at the end.
-        // this results in anchor.href being the same in each call (wrong)
-        $.ajax({
-            url: anchor.href,
-            type: 'GET',
-            async: false,
-            success: function(data) {
-                var state = $(data).find("div.state")[0].classList[1];
+        dict[anchor] = checklist[i];
 
-                switch (state) {
-                    case "state-open":
-                        anchor.insertAdjacentHTML("beforebegin", makeStateButton("Open"));
-                        anchor.insertAdjacentHTML("beforebegin", "<span> </span>");
-                        break;
-                    case "state-closed":
-                        anchor.insertAdjacentHTML("beforebegin", makeStateButton("Closed"));
-                        anchor.insertAdjacentHTML("beforebegin", "<span> </span>");
-                        break;
-                    case "state-merged":
-                        anchor.insertAdjacentHTML("beforebegin", makeStateButton("Merged"));
-                        anchor.insertAdjacentHTML("beforebegin", "<span> </span>");
-                        break;
+        (function(anchor) {
+            $.ajax({
+                url: anchor.href,
+                type: 'GET',
+                context: checklist[i],
+                success: function(data) {
+                    // do check for pull vs. issue
+                    var state = $(data).find("div.state")[0].classList[1];
+                    
+                    // console.log("key 2 " + anchor.href);        
+                    // console.log(anchor.href.search("issue"));
+                    
+                    // console.log($(this)[0])
+                    switch (state) {
+                        case "state-open":
+                            anchor.insertAdjacentHTML("beforebegin", makeStateButton("Open"));
+                            anchor.insertAdjacentHTML("beforebegin", "<span> </span>");
+                            console.log("openn");
+                            break;
+                        case "state-closed":
+                            anchor.insertAdjacentHTML("beforebegin", makeStateButton("Closed"));
+                            anchor.insertAdjacentHTML("beforebegin", "<span> </span>");
+                            console.log("closedd")
+                            break;
+                        case "state-merged":
+                            anchor.insertAdjacentHTML("beforebegin", makeStateButton("Merged"));
+                            anchor.insertAdjacentHTML("beforebegin", "<span> </span>");
+                            console.log("mergedd")
+                            break;
+                    }
                 }
-            }
-        });
+            });
+        })(anchor);
     };
 
+    // for(var key in dict) {
+        // console.log("key 1" + key);
+        // console.log(dict[key]);
+
+        // (function(key) {
+        //     $.ajax({
+        //         url: key,
+        //         type: 'GET',
+        //         context: dict[key],
+        //         success: function(data) {
+        //             // do check for pull vs. issue
+        //             var state = $(data).find("div.state")[0].classList[1];
+                    
+        //             console.log("key 2" + key);        
+        //             console.log(key.search("issue"));
+
+        //             if(key.search("issue") > 0) {
+        //                 //issue
+        //             } else {
+        //                 //pull request
+        //             }
+
+        //             console.log($(this)[0])
+        //             switch (state) {
+        //                 case "state-open":
+        //                     // anchor.insertAdjacentHTML("beforebegin", makeStateButton("Open"));
+        //                     // anchor.insertAdjacentHTML("beforebegin", "<span> </span>");
+        //                     console.log("openn");
+        //                     break;
+        //                 case "state-closed":
+        //                     // anchor.insertAdjacentHTML("beforebegin", makeStateButton("Closed"));
+        //                     // anchor.insertAdjacentHTML("beforebegin", "<span> </span>");
+        //                     console.log("closedd")
+        //                     break;
+        //                 case "state-merged":
+        //                     // anchor.insertAdjacentHTML("beforebegin", makeStateButton("Merged"));
+        //                     // anchor.insertAdjacentHTML("beforebegin", "<span> </span>");
+        //                     console.log("mergedd")
+        //                     break;
+        //             }
+        //         }
+        //     });
+        // })(key);
+    // }
     console.log("Finished!")
 }
