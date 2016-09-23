487bcb75f9761bb9519e66c19e1ce0ad83b01f8e
JamesEarle <j_earle@hotmail.com>
2016-09-23 15:09:43 -0700

    updated for back button press

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function makeStateButton(state, isIssue) {
         "Merged": "octicon-git-pull-request"
     }
 
-    return "<span style='width:83.59px;' class='state state-" + state.toLowerCase() + "'> \
+    return "<span id='injected-state'><span style='width:83.59px;' class='state state-" + state.toLowerCase() + "'> \
             <svg aria-hidden='true' class='octicon " + iconClass[state] +"' height='16' version='1.1' viewBox='0 0 14 16' width='14'><path d='M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1\ 3H6v5h2V4zm0 6H6v2h2v-2z'></path></svg>\
-            " + state + "</span>";
+            " + state + "</span></span>";
 }
 
 // Might want to refresh the page when an input is checked to maintain the state buttons
@@ -29,11 +29,15 @@ function makeStateButton(state, isIssue) {
 
 
 function collectLinks() {
+
+    // Just in case you navigate and the state buttons already exist, remove them
+    $("span#injected-state").remove();
+
     // Find task list
     var checklist = document.querySelectorAll("a.issue-link");
 
     if (checklist.length != 0) {
-        console.log("Fetching information on issues and pull requests...");
+        // console.log("Fetching information on issues and pull requests...");
 
         // Collect children elements for the containing div
         for (var i = 0; i < checklist.length; i++) {
@@ -64,25 +68,29 @@ function collectLinks() {
                 });
             })(checklist[i]);
         };
-        console.log("Finished!");
+        // console.log("Finished!");
     }
 }
 
+// window.onbeforeunload = function (e) {
+//             var e = e || window.event;
+//             var msg = "Do you really want to leave this page?"
+
+//             // For IE and Firefox
+//             if (e) {
+//                 e.returnValue = msg;
+//             }
+
+//             // For Safari / chrome
+//             return msg;
+//          };
+
+// Call once when we start
 $(document).ready(function() {
-    // Call once, in case we start on an issues page
     collectLinks();  
 });
 
-// For the rest of the session wait for navigation
-var oldUrl = window.location.href;
-setInterval(function() {
-    // Detect navigation in GitHub's "one-page" structure
-    if(window.location.href != oldUrl) {
-        oldUrl = window.location.href;
-        // if collectLinks() is called too quickly the HTML won't have
-        // rendered properly and it will say it has found no a.issue-link
+// And call again for every page navigation.
+document.addEventListener("pjax:end", function() {
         collectLinks();
-    }
-}, 1000);  
-
-
+});
